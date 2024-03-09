65971cbca5318c581097cb744189d3e993bf9871
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2024-03-09 10:03:50 -0300

    finish chapter 3 and started chapter 4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, HttpResponse};
-use chrono::Utc;
 use sqlx::PgPool;
+use chrono::Utc;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,23 +10,27 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
-            "#,
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    .execute(pool.as_ref())
-    .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
+    .execute(pool.get_ref())
+    .await {
+        Ok(_) =>  {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::info!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
+    
 }
